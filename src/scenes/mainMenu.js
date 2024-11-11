292ff194b699eeacfc60a05a0820c73e4718d4f7
Romyadhy <romyadhy@student.undiksha.ac.id>
2024-11-11 14:39:46 +0800

    add home sence

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -17,6 +17,18 @@ export default function mainMenu(){
         k.add([k.sprite("platforms"), k.pos(platformWidth *4, 450), k.scale(4)]),
     ];
 
+    k.add([
+        k.text("SONIC RUN GAME", {font: "mania", size: 96}),
+        k.pos(k.center().x, 200),
+        k.anchor("center")
+    ]);
+
+    k.add([
+        k.text("Press The Space/Click/Touch to play ||ENJOY", {font: "mania", size: 32}),
+        k.pos(k.center().x, k.center().y - 200),
+        k.anchor("center"),
+    ]);
+
     makeSonic(k.vec2(200, 745))
 
     const gameSpeed = 4000;
