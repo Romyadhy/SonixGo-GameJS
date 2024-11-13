3c0ad9a0db58edd1a18edd6850fcc2cc4201f1d6
Romyadhy <romyadhy@student.undiksha.ac.id>
2024-11-13 13:18:25 +0800

    add enemy

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,5 +1,6 @@
 import k from "../kaplayCTX";
 import { makeSonic } from "../entities/sonic";
+import { createMotobug } from "../entities/motobug";
 
 
 
@@ -22,11 +23,47 @@ export default function game() {
    //Call the method to jump
    sonic.setJumpSonic();
    sonic.setEventsSonic();
+   sonic.onCollide("enemy", (enemy) => {
+    if(!sonic.isGrounded()) {
+        k.play("destroy", {volume: 0.5});
+        k.play("hyper-ring", {volume: 0.5});
+        k.destroy(enemy);
+        sonic.play("jump");
+        sonic.jump();
+        return;
+    }
+
+    k.play("hurt", {volume: 0.3});
+    k.go("gameover");
+
+   });
 
     let gameSpeed = 300;
     k.loop(1, () => {
         gameSpeed += 50;
     });
+
+    // call the motobug(enemy)
+    const spawnMotobug = () => {
+        const motoBug = createMotobug(k.vec2(1950, 773));
+        motoBug.onUpdate(() => {
+            if(gameSpeed < 3000) {
+                motoBug.move(-(gameSpeed + 300), 0);
+                return;
+            }
+            motoBug.move(-gameSpeed, 0);
+
+        });
+
+        motoBug.onExitScreen(() => {
+            if(motoBug.pos.x < 0) k.destroy(motoBug);
+        });
+
+        const timeWait = k.rand(0.5, 2.5);
+        k.wait(timeWait, spawnMotobug); //rekurdif function for infinite spwan enemy
+
+    };
+    spawnMotobug();
 
     k.add([
         k.opacity(0),
@@ -47,13 +84,13 @@ export default function game() {
 
          // for jump effect
          bgPieces[0].moveTo(bgPieces[0].pos.x, -sonic.pos.y / 10 - 50);
-         bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
-
-        if (platforms[1].pos.x < 0) {
-            platforms[0].moveTo(platforms[1].pos.x + platformWidth * 4, 450);
-            platforms.push(platforms.shift());
+         
+         if (platforms[1].pos.x < 0) {
+             platforms[0].moveTo(platforms[1].pos.x + platformWidth * 4, 450);
+             platforms.push(platforms.shift());
             }
-        platforms[0].move(-gameSpeed, 0),
+            platforms[0].move(-gameSpeed, 0),
+            bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
         platforms[1].moveTo(platforms[0].pos.x + platformWidth * 4, 450);
 
     });
